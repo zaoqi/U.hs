186fc508b2923b16dac9ec4da2da10e2511fc01c
zaoqi <zaomir@outlook.com>
2017-07-04 19:17:17 +0800

    Eq,Show,修改創建和轉換

--- Data/U.hs
+++ Data/U.hs
@@ -12,8 +12,8 @@
 
 --You should have received a copy of the GNU Affero General Public License
 --along with this program.  If not, see <http://www.gnu.org/licenses/>.
-{-# LANGUAGE DataKinds, TypeOperators, KindSignatures, GADTs, MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, AllowAmbiguousTypes
- #-}
+{-# LANGUAGE DataKinds, TypeOperators, KindSignatures, GADTs, MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, AllowAmbiguousTypes,
+UndecidableInstances, IncoherentInstances, NoMonomorphismRestriction #-}
 module Data.U (
     U(),
     t,
@@ -24,18 +24,34 @@ data U :: [*] -> * where
     UOne :: x -> U (x : xs)
     USucc :: U xs -> U (x : xs)
 
-class T a b where
-    t :: a -> b
-instance T a a where
-    t = id
-instance T (U xs) (U (x : xs)) where
-    t = USucc
-instance T (U (x : y : xs)) (U (y : x : xs)) where
-    t (UOne x) = USucc (UOne x)
-    t (USucc (UOne x)) = UOne x
-    t (USucc (USucc xs)) = USucc (USucc xs)
-instance T (U xs) (U ys) => T (U (x : xs)) (U (x : ys)) where
-    t (UOne x) = UOne x
-    t (USucc xs) = USucc (t xs)
+class T1 a b where
+    t1 :: (U a) -> (U b)
+instance T1 a a where
+    t1 = id
+instance T1 xs (x : xs) where
+    t1 = USucc
+instance T1 (x : y : xs) (y : x : xs) where
+    t1 (UOne x) = USucc (UOne x)
+    t1 (USucc (UOne x)) = UOne x
+    t1 (USucc (USucc xs)) = USucc (USucc xs)
+instance T1 xs ys => T1 (x : xs) (x : ys) where
+    t1 (UOne x) = UOne x
+    t1 (USucc xs) = USucc (t1 xs)
+t = t1 . t1 . t1 . t1 . t1 . t1 . t1 . t1
 
-u x = t (UOne x)
+uone :: a -> U '[a]
+uone = UOne
+u x = t (uone x)
+
+instance Eq x => Eq (U '[x]) where
+    UOne x == UOne y = x == y
+instance (Eq x, Eq (U xs)) => Eq (U (x : xs)) where
+    UOne x == UOne y = x == y
+    USucc xs == USucc ys = xs == ys
+    _ == _ = False
+
+instance Show x => Show (U '[x]) where
+    show (UOne x) = "(u " ++ showsPrec 11 x ")"
+instance (Show x, Show (U xs)) => Show (U (x : xs)) where
+    show (UOne x) = "(u " ++ showsPrec 11 x ")"
+    show (USucc xs) = show xs
